c832bf8d85ff4d891618d3438cb3c680dde1f8f7
karencumlat <karencumlat@gmail.com>
2020-11-05 10:26:44 -0800

    Add dress my feet feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+
+import "./helpers/fontawesome";
+import { sneakers } from "./helpers/data";
 
 function App() {
+  const [dressShoes, setDressShoes] = React.useState(0);
+  const [dressFeet, setDressFeet] = React.useState("");
+  const [sneakersName, setSneakerName] = React.useState("");
+
+  React.useEffect(() => {
+    const dressFeetUrl = `./images/${sneakers[dressShoes].tag}.svg`;
+    setDressFeet(process.env.PUBLIC_URL + dressFeetUrl);
+    setSneakerName(sneakers[dressShoes].name);
+  }, [dressShoes]);
+
+  function nextSneakers() {
+    sneakers.forEach((curr, index) => {
+      if (index === dressShoes) {
+        index > sneakers.length - 2
+          ? setDressShoes(0)
+          : setDressShoes(index + 1);
+      }
+    });
+  }
+
+  function prevSneakers() {
+    sneakers.forEach((curr, index) => {
+      if (index === dressShoes) {
+        console.log(index);
+        index < 1
+          ? setDressShoes(sneakers.length - 1)
+          : setDressShoes(index - 1);
+      }
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="sneakers-feature">
+        <div className="sneakers-feature--base">
+          <button
+            onClick={prevSneakers}
+            className="primary-btn sneakers-feature--btn"
+          >
+            <FontAwesomeIcon icon="chevron-left" size="4x" />
+          </button>
+          <img
+            src={process.env.PUBLIC_URL + "./images/leg.svg"}
+            alt="Leg with sock"
+            className="sneakers-feature--leg"
+          />
+          <button
+            onClick={nextSneakers}
+            className="primary-btn sneakers-feature--btn"
+          >
+            <FontAwesomeIcon icon="chevron-right" size="4x" />
+          </button>
+        </div>
+
+        <img
+          src={dressFeet}
+          alt="Dress My Feet"
+          className="sneakers-feature--dress-my-feet"
+        />
+        <h2 className="sneakers-feature--name">{sneakersName}</h2>
+      </section>
+
+      <nav className="sneakers-gallery">
+        <div className="sneakers-gallery--card-group">
+          {sneakers.map((sneaks) => {
+            const imgUrl = `./images/${sneaks.tag}.svg`;
+            return (
+              <div className="sneakers-card" key={sneaks.tag}>
+                <button
+                  onClick={() => setDressShoes(sneakers.indexOf(sneaks))}
+                  className="sneakers-card--button"
+                >
+                  <img
+                    src={process.env.PUBLIC_URL + imgUrl}
+                    alt={sneaks.name}
+                    className="sneakers-card--image"
+                  />
+                  <h2 className="sneakers-card--name">{sneaks.name}</h2>
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </nav>
     </div>
   );
 }
---
+++ src/helpers/data.js
@@ -0,0 +1,57 @@
+export const sneakers = [
+  {
+    name: "Dress My Feet",
+    color: "#FFFFFF",
+    tag: "basic",
+  },
+  {
+    name: "Noodles are Forever",
+    color: "#D4CBC8",
+    tag: "noodles",
+  },
+  {
+    name: "Magic Bacon Ride",
+    color: "#FCE397",
+    tag: "bacon",
+  },
+  {
+    name: "Blueberries",
+    color: "#787896",
+    tag: "blueberry",
+  },
+  {
+    name: "Kiwi Seeds",
+    color: "#F0EFEF",
+    tag: "kiwi",
+  },
+  {
+    name: "Let's Get Eggy With It",
+    color: "FBC4C4",
+    tag: "egg",
+  },
+  {
+    name: "Bright Bunch",
+    color: "#EE925D",
+    tag: "banana",
+  },
+  {
+    name: "Summer Paradise",
+    color: "#228FBE",
+    tag: "pops",
+  },
+  {
+    name: " Playin in the Sprinkler",
+    color: "FF3333",
+    tag: "sprinkle",
+  },
+  {
+    name: "Boatload",
+    color: "#F7CC9C",
+    tag: "sushi",
+  },
+  {
+    name: "Cherry Red",
+    color: "#699B3A",
+    tag: "cherry",
+  },
+];
